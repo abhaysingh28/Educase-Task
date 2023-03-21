98bc432dc52eaa6bba242618d9500d447bd7b77c
अभय सिंह <99662885+abhaysingh28@users.noreply.github.com>
2023-03-21 11:36:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+     <div className="main">
+        <div className="mainContent">
+            <div className="img"><img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+                    alt=""/></div>
+            <div className="randomText">
+                <h4>some random text,some random <br /> text,some random text,some random text</h4>
+            </div>
+            <div className="loginSignup">
+                <div className="login">
+                    <h4>Login</h4>
+                </div>
+                <div className="signup">
+                    <h4>Signup</h4>
+                </div>
+            </div>
+            <div className="inputTags">
+                <input type="text" placeholder="Email Address"/>
+                <input type="text" placeholder="Password"/>
+                <button>LOGIN</button>
+                <h5>Forgot Password?</h5>
+                <h6>or login with</h6>
+            </div>
+            <div className="Links">
+                <div className="Link">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48">
+                        <path fill="#FFC107"
+                            d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z">
+                        </path>
+                        <path fill="#FF3D00"
+                            d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z">
+                        </path>
+                        <path fill="#4CAF50"
+                            d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z">
+                        </path>
+                        <path fill="#1976D2"
+                            d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z">
+                        </path>
+                    </svg>
+                </div>
+                <div className="Link">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path fill="none" d="M0 0h24v24H0z" />
+                        <path
+                            d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4v-8.5z" />
+                    </svg>
+                </div>
+                <div className="Link">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path fill="none" d="M0 0h24v24H0z" />
+                        <path
+                            d="M22.162 5.656a8.384 8.384 0 0 1-2.402.658A4.196 4.196 0 0 0 21.6 4c-.82.488-1.719.83-2.656 1.015a4.182 4.182 0 0 0-7.126 3.814 11.874 11.874 0 0 1-8.62-4.37 4.168 4.168 0 0 0-.566 2.103c0 1.45.738 2.731 1.86 3.481a4.168 4.168 0 0 1-1.894-.523v.052a4.185 4.185 0 0 0 3.355 4.101 4.21 4.21 0 0 1-1.89.072A4.185 4.185 0 0 0 7.97 16.65a8.394 8.394 0 0 1-6.191 1.732 11.83 11.83 0 0 0 6.41 1.88c7.693 0 11.9-6.373 11.9-11.9 0-.18-.005-.362-.013-.54a8.496 8.496 0 0 0 2.087-2.165z" />
+                    </svg>
+                </div>
+
+            </div>
+            <h2>Don't have an account? <span>Create new now!</span></h2>
+            <h2>By signing up, you are agree with our <span style={{borderBottom:"1.3px solid rgb(249, 98, 199)"}} >Terms
+                    & Conditons</span></h2>
+
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
